8d253c62c82fe499ed33b39f4c652cd2a68d9aa2
Passion-Logan <wangdadamailbox@163.com>
2020-04-08 21:37:57 +0800

    添加注释

--- src/main/java/com/cody/controller/LogionController.java
+++ src/main/java/com/cody/controller/LogionController.java
@@ -31,9 +31,20 @@ public class LogionController {
     @Value("${server.port}")
     private int prot;
 
+    /**
+     * redirect: 跳转 url（app 授权之后，跳转的页面）
+     * subscribe: 订阅url（用户访问会这个url，开启长连接，接收服务端推送的扫码，登陆事件）
+     * qrcode: base64格式的二维码图片
+     *
+     * @param data
+     * @return
+     * @throws IOException
+     * @throws WriterException
+     */
     @GetMapping("login")
     public String qr(Map<String, Object> data) throws IOException, WriterException {
         String id = UUID.randomUUID().toString();
+        // 本机测试时不能用127.0.0.1
         String ip = IPUtils.getLocalIP();
 
         String pref = "http://" + ip + ":" + prot + "/";
